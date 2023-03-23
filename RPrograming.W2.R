de1135751fc22c9a185912e340ebcf648b1e60f6
Udi Zabot <udi.zabott@gmail.com>
2023-03-23 14:22:49 -0400

    Update

--- RPrograming.W2.R
+++ RPrograming.W2.R
@@ -20,15 +20,9 @@ pollutantmean <- function(directory, pollutant, ID = 1:332) {
 
 pollutantmean("C:\\Career\\Programming\\specdata", "sulfate", 1:10)
 
+pollutantmean("C:\\Career\\Programming\\specdata", "nitrate", 70:72)
+
 pollutantmean("C:\\Career\\Programming\\specdata", "nitrate", 23)
-
-
-
-
-
-
-
-
 
 
 
@@ -40,13 +34,15 @@ complete <- function(directory, ID = 1:332) {
     data <- list.files(pattern = "*.csv") %>% 
         map_df(~fread(.)) %>% 
         filter( . , 
-                ID >= id[1] & ID <= id[length(id)]) %>%
+                ID %in% id) %>%
         group_by(ID) %>%
-        summarise(nobs = sum(!is.na(sulfate) & !is.na(nitrate)))
-    
+        summarise(nobs = sum(!is.na(sulfate) & !is.na(nitrate))) %>%
+        arrange( . , id)
     table = as.data.frame(data)  
     return(table)
 }
+
+complete("C:\\Career\\Programming\\specdata", 1)
 
 complete("C:\\Career\\Programming\\specdata", c(2, 3))
 
@@ -54,6 +50,42 @@ complete("C:\\Career\\Programming\\specdata", c(2, 4, 8, 10, 12))
 
 complete("C:\\Career\\Programming\\specdata", 30:25)
 
+
+
+# Correlation -------------------------------------------------------------
+
+
+corr <- function(directory, threshold = 0) {
+    "directory" = directory
+    Table <- complete(directory) 
+    threshold = threshold
+    Table2 <- filter(Table, nobs > threshold)
+    index <- Table2$ID
+    data <- list.files(pattern = "*.csv") %>% 
+        map_df(~fread(.)) %>% 
+        filter( . , 
+                ID %in% index, 
+                !is.na(sulfate) & !is.na(nitrate)) %>%
+        group_by(ID) %>%
+        summarize(corr = as.numeric(cor(sulfate, nitrate)))
+    corr = data$corr
+    return(corr)
+}
+
+cr <- corr("C:\\Career\\Programming\\specdata", 150)
+head(cr)
+summary(cr)
+
+cr <- corr("C:\\Career\\Programming\\specdata", 400)
+head(cr)
+summary(cr)
+
+cr <- corr("C:\\Career\\Programming\\specdata", 5000)
+summary(cr)
+length(cr)
+
+cr <- corr("C:\\Career\\Programming\\specdata")
+length(cr)
 
 
 
